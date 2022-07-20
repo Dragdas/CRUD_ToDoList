7a41d7f792b032bd9a8eb65b58eaef59bbb03f0e
Krzysiek <krzysztofkulpacb@gmail.com>
2022-07-20 12:19:47 +0200

    21.4 runcrud and showtasks scripts added

---
+++ showtasks.bat
@@ -0,0 +1,18 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto openbrowser
+echo.
+echo There were errors
+goto fail
+
+
+:openbrowser
+start "" http://localhost:8080/crud/v1/tasks
+goto end
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Show tasks script finished
--- src/main/java/com/example/crud_todolist/CrudToDoListApplication.java
+++ src/main/java/com/example/crud_todolist/CrudToDoListApplication.java
@@ -2,12 +2,19 @@ package com.example.crud_todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrudToDoListApplication {
+public class CrudToDoListApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CrudToDoListApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(CrudToDoListApplication.class);
     }
 
 }
