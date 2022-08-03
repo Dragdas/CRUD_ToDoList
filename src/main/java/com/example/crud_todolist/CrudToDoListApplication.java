946033e1d381bdaedefd3a559c82f3660bccb23e
Krzysiek <krzysztofkulpacb@gmail.com>
2022-08-03 18:23:55 +0200

    23.2 swagger issue

--- src/main/java/com/example/crud_todolist/CrudToDoListApplication.java
+++ src/main/java/com/example/crud_todolist/CrudToDoListApplication.java
@@ -2,11 +2,13 @@ package com.example.crud_todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-//public class CrudToDoListApplication extends SpringBootServletInitializer {
+/*@EnableSwagger2
+@EnableWebMvc*/
 public class CrudToDoListApplication {
 
     public static void main(String[] args) {
