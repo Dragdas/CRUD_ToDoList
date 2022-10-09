675f067c0a37a485855d56a2a98680608015b9de
Krzysiek <krzysztofkulpacb@gmail.com>
2022-10-09 14:21:18 +0200

    32.3 task- edit mail template

--- src/main/java/com/example/crud_todolist/service/MailCreatorService.java
+++ src/main/java/com/example/crud_todolist/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.example.crud_todolist.service;
 
+import com.example.crud_todolist.config.AdminConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,17 @@ public class MailCreatorService {
 
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
+    private AdminConfig adminConfig;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://dragdas.github.io/ToDoList-Front/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "Kind regards!");
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("preview", "New card added!");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
